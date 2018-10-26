49a7b4557718046a8c807709ddfe87b3442f0b80
seoheedong <starkay@naver.com>
2018-10-26 09:58:31 +0900

    first commit

---
+++ src/Operators.java
@@ -0,0 +1,13 @@
+public class Operators {
+    public void computer() {
+        System.out.println(1+2);
+        System.out.println(3-1);
+        System.out.println(5*4);
+
+        // special case
+        System.out.println(5/2+"."+5%2);
+        System.out.println(5%2);
+
+    }
+}
+
